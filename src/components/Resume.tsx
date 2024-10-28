55b084a5576504cce2511113e52d6e4b66c3a71f
Winston Lam <winston.lam@outlook.com>
2024-10-28 19:51:17 +0100

    Updated Styling and Content

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,160 +1,274 @@
-
 import { useState, useEffect } from "react";
 import Selector from "./Selector";
 
 import "../styles/Resume.css";
 
 const Resume = () => {
-    const [education, setEducation] = useState<number>(0);
-    const [work, setWork] = useState<number>(0);
-    const [visible, setVisible] = useState<boolean>(true);
+  const [education, setEducation] = useState<number>(0);
+  const [work, setWork] = useState<number>(0);
+  const [visible, setVisible] = useState<boolean>(true);
 
-    // Trigger fade out when education changes
-    useEffect(() => {
-        setVisible(false);
-    }, [education]);
+  // Trigger fade out when education changes
+  useEffect(() => {
+    setVisible(false);
+  }, [education]);
 
+  return (
+    <div className="resume-inner">
+      <div className="resume-inner-title">
+        <h1>
+          02. <span className="highlight"> Experience </span>
+        </h1>
+      </div>
+      <div className="resume-box1">
+        <div className="resume-box1-title">
+          <h1>My Education</h1>
+        </div>
+        <div className="resume-box1-selector">
+          <Selector
+            boxNames={["MSc SE", "BSc IS", "VWO"]}
+            onChange={setEducation}
+          />
+        </div>
 
-    return (
-        <div className="resume-inner">
-            <div className="resume-inner-title">
-                <h1>02. <span className="highlight"> Experience </span></h1>
+        <div className="resume-box1-inner">
+          <div className="resume-box1-inner-title">
+            {/* MSc SE */}
+            <div
+              className={`resume-box1-content${
+                education === 0 ? "-active" : ""
+              }`}
+            >
+              <h1>
+                <span className="highlight1">MSc Software Engineering </span>-{" "}
+                <span className="highlight">University of Amsterdam</span>
+              </h1>
+              <h3>September 2021 - June 2023</h3>
             </div>
-            <div className="resume-box1">
-                <div className="resume-box1-title">
-                    <h1>My Education</h1>
-                </div>
-                <div className="resume-box1-selector">
-                    <Selector boxNames={["VWO", "BSc IS", "MSc SE"]} onChange={setEducation} />
-                </div>
 
-                <div className="resume-box1-inner" >
-                    <div className="resume-box1-inner-title">
-
-
-
-                        <div className={`resume-box1-content${education === 0 ? '-active' : ''}`} >
-                            <h1> <span className="highlight1">VWO Degree </span>- <span className="highlight">Veluws College Walterbosch</span></h1>
-                            <h4>September 2011 - June 2017</h4>
-                        </div>
-
-                        <div className={`resume-box1-content${education === 1 ? '-active' : ''}`} >
-                            <h1> <span className="highlight1">BSc Information Science </span>- <span className="highlight">University of Amsterdam</span></h1>
-                            <h4>Setpember 2017 - April 2021</h4>
-                        </div>
-
-                        <div className={`resume-box1-content${education === 2 ? '-active' : ''}`} >
-                            <h1> <span className="highlight1">MSc Software Engineering </span>- <span className="highlight">University of Amsterdam</span></h1>
-                            <h4>September 2021 - June 2023</h4>
-                        </div>
-                    </div>
-
-
-                    <div className={`resume-box1-inner-content${education === 0 ? '-active' : ''}`} >
-                        <dl>
-                            <dd>Attended the highest high school education in The Netherlands</dd>
-                            <dd>Continued having classes in Java, HTML, CSS, Adobe programs and more</dd>
-                            <dd>Graduated with a GPA of 7</dd>
-                        </dl>
-                    </div>
-
-                    <div className={`resume-box1-inner-content${education === 1 ? '-active' : ''}`} >
-                        <dl>
-                            <dd>Studied Information Science at the University of Amsterdam</dd>
-                            <dd>Attended courses about Information System Modelling, Machine Learning, Python, Big Data and SQL</dd>
-                            <dd>Attended a minor program E-commerce and Online Business at the Vrije Universiteit Amsterdam</dd>
-                        </dl>
-                    </div>
-
-                    <div className={`resume-box1-inner-content${education === 2 ? '-active' : ''}`} >
-                        <dl>
-                            <dd>Studied Software Engineering at the University of Amsterdam</dd>
-                            <dd>Attended courses about Compiler Construction, Cloud Computing, Software Testing and more</dd>
-                            <dd>Graduated with a GPA of 7.5</dd>
-
-                        </dl>
-                    </div>
-
-
-
-                </div>
-
+            {/* BSc IS */}
+            <div
+              className={`resume-box1-content${
+                education === 1 ? "-active" : ""
+              }`}
+            >
+              <h1>
+                <span className="highlight1">BSc Information Science </span>-{" "}
+                <span className="highlight">University of Amsterdam</span>
+              </h1>
+              <h3>September 2017 - April 2021</h3>
             </div>
-            <div className="resume-box2">
-                <div className="resume-box1-title">
-                    <h1>Work Experience</h1>
-                </div>
-                <div className="resume-box1-selector">
-                    <Selector boxNames={["SAR", "CargoLedger", "Originals"]} onChange={setWork} />
-                </div>
 
-                <div className="resume-box1-inner" >
-                    <div className="resume-box1-inner-title">
-
-
-
-                        <div className={`resume-box1-content${work === 0 ? '-active' : ''}`} >
-                            <h1> <span className="highlight1">Schiphol Airport Retail </span>- <span className="highlight">Sales Representitive</span></h1>
-                            <h4>September 2017 - February 2020</h4>
-                        </div>
-                        <div className={`resume-box1-content${work === 1 ? '-active' : ''}`} >
-                            <h1> <span className="highlight1">CargoLedger </span>- <span className="highlight">Blockchain Developer Intern</span></h1>
-                            <h4>April 2023 - August 2023</h4>
-                        </div>
-
-
-                        <div className={`resume-box1-content${work === 2 ? '-active' : ''}`} >
-                            <h1> <span className="highlight1">Originals </span>- <span className="highlight">Co-Founder & Chief of Operations</span></h1>
-                            <h4>December 2019 - Present</h4>
-                        </div>
-
-                        {/* <div className={`resume-box1-content${work === 2 ? '-active' : ''}`} >
-                            <h1> <span className="highlight1">MSc Software Engineering </span>- <span className="highlight">University of Amsterdam</span></h1>
-                            <h4>September 2021 - June 2023</h4>
-                        </div> */}
-                    </div>
-
-
-                    <div className={`resume-box1-inner-content${work === 0 ? '-active' : ''}`} >
-
-                        <dl>
-                            <dd>Provided high-quality customer service in a fast-paced airport setting, demonstrating effective communication and problem-solving skills.
-                                Managed cash transactions and inventory with keen attention to detail. Collaborated effectively within a team to meet sales goals.
-                                Developed resilience and adaptability skills, transferable to software engineering roles.</dd>
-                        </dl>
-                    </div>
-                    <div className={`resume-box1-inner-content${work === 1 ? '-active' : ''}`} >
-                        <dl>
-                            <dd>At CargoLedger, a premier logistics firm known for blockchain solutions, I researched consensus mechanisms with an emphasis 
-                                on partial migration strategies. I sharpened my skills in Solidity, smart contract development, and Ethereum-Polygon communication, 
-                                contributing to the company's advanced blockchain integration in logistics.</dd>
-                        </dl>
-                    </div>
-
-                    <div className={`resume-box1-inner-content${work === 2 ? '-active' : ''}`} >
-                        <dl>
-                            <dd>Co-founded and currently oversee a thriving restaurant, applying strategic planning and leadership skills. Implement various
-                                software systems for workflow optimization and operational efficiency. Maintain high standards in daily operations while
-                                ensuring customer satisfaction. Utilize multitasking and team management abilities, fostering skills transferable to software
-                                engineering and project management.</dd>
-                        </dl>
-                    </div>
-
-                    {/* {education === 2 &&
-                            <dl>
-                                <dd>Studied Software Engineering at the University of Amsterdam</dd>
-                                <dd>Attended courses about Compiler Construction, Cyberphysical Software Systems, Cloud Computing,<br /> Software Testing, C, Haskell, and Javascript</dd>
-
-                            </dl>
-                        } */}
-
-
-
-                </div>
-
+            {/* VWO */}
+            <div
+              className={`resume-box1-content${
+                education === 2 ? "-active" : ""
+              }`}
+            >
+              <h1>
+                <span className="highlight1">VWO Degree </span>-{" "}
+                <span className="highlight">Veluws College Walterbosch</span>
+              </h1>
+              <h3>September 2011 - June 2017</h3>
             </div>
-        </div>)
+          </div>
 
-}
+          <div
+            className={`resume-box1-inner-content${
+              education === 0 ? "-active" : ""
+            }`}
+          >
+            <dl>
+              <dd>
+                Studied <span className="highlight">Software Engineering</span>{" "}
+                at the University of Amsterdam
+              </dd>
+              <dd className="highlight">
+                Attended courses about{" "}
+                <span className="highlight">Compiler Construction</span>,{" "}
+                <span className="highlight">Cloud Computing</span>,{" "}
+                <span className="highlight">Software Testing</span>, and more
+              </dd>
+              <dd>
+                Graduated with a GPA of <span className="highlight">7.5</span>
+              </dd>
+            </dl>
+          </div>
+
+          <div
+            className={`resume-box1-inner-content${
+              education === 1 ? "-active" : ""
+            }`}
+          >
+            <dl>
+              <dd>
+                Studied <span className="highlight">Information Science</span>{" "}
+                at the University of Amsterdam
+              </dd>
+              <dd className="highlight">
+                Attended courses about{" "}
+                <span className="highlight">Information System Modelling</span>,{" "}
+                <span className="highlight">Machine Learning</span>,{" "}
+                <span className="highlight">Python</span>,{" "}
+                <span className="highlight">Big Data</span>, and{" "}
+                <span className="highlight">SQL</span>
+              </dd>
+              <dd>
+                Attended a minor program in{" "}
+                <span className="highlight">
+                  E-commerce and Online Business
+                </span>{" "}
+                at the Vrije Universiteit Amsterdam
+              </dd>
+            </dl>
+          </div>
+
+          <div
+            className={`resume-box1-inner-content${
+              education === 2 ? "-active" : ""
+            }`}
+          >
+            <dl>
+              <dd>
+                Attended the highest high school education in The Netherlands
+              </dd>
+              <dd className="highlight">
+                Continued having classes in{" "}
+                <span className="highlight">Java</span>,{" "}
+                <span className="highlight">HTML</span>,{" "}
+                <span className="highlight">CSS</span>, Adobe programs, and more
+              </dd>
+              <dd>
+                Graduated with a GPA of <span className="highlight">7</span>
+              </dd>
+            </dl>
+          </div>
+        </div>
+      </div>
+
+      <div className="resume-box2">
+        <div className="resume-box1-title">
+          <h1>Work Experience</h1>
+        </div>
+        <div className="resume-box1-selector">
+          <Selector
+            boxNames={["ING", "Originals", "CargoLedger"]}
+            onChange={setWork}
+          />
+        </div>
+
+        <div className="resume-box1-inner">
+          <div className="resume-box1-inner-title">
+            {/* ING */}
+            <div
+              className={`resume-box1-content${work === 0 ? "-active" : ""}`}
+            >
+              <h1>
+                <span className="highlight1">ING </span>-{" "}
+                <span className="highlight">Software Engineer</span>
+              </h1>
+              <h3>September 2024 - Present</h3>
+            </div>
+
+            {/* Originals */}
+            <div
+              className={`resume-box1-content${work === 1 ? "-active" : ""}`}
+            >
+              <h1>
+                <span className="highlight1">Originals </span>-{" "}
+                <span className="highlight">
+                  Co-Founder & Chief of Operations
+                </span>
+              </h1>
+              <h3>December 2019 - Present</h3>
+            </div>
+
+            {/* CargoLedger */}
+            <div
+              className={`resume-box1-content${work === 2 ? "-active" : ""}`}
+            >
+              <h1>
+                <span className="highlight1">CargoLedger </span>-{" "}
+                <span className="highlight">Blockchain Developer Intern</span>
+              </h1>
+              <h3>April 2023 - August 2023</h3>
+            </div>
+          </div>
+
+          <div
+            className={`resume-box1-inner-content${
+              work === 0 ? "-active" : ""
+            }`}
+          >
+            <dl>
+              <dd>
+                At <span className="highlight">ING</span>, a global financial
+                institution renowned for innovation in banking, I work as a{" "}
+                <span className="highlight">Software Engineer</span> focusing on
+                developing and optimizing financial software systems. Leveraging
+                various technologies and{" "}
+                <span className="highlight">microservices architecture</span>, I
+                contribute to building secure and scalable applications.
+                Collaborate with cross-functional teams to implement new
+                features, enhance system performance, and ensure compliance with
+                industry regulations. This role has enriched my expertise in{" "}
+                <span className="highlight">backend development</span>,{" "}
+                <span className="highlight">cloud services</span>, and{" "}
+                <span className="highlight">cybersecurity practices</span>{" "}
+                within the finance sector.
+              </dd>
+            </dl>
+          </div>
+
+          <div
+            className={`resume-box1-inner-content${
+              work === 1 ? "-active" : ""
+            }`}
+          >
+            <dl>
+              <dd>
+                Co-founded and currently oversee a thriving restaurant, applying
+                strategic planning and leadership skills. Implement various{" "}
+                <span className="highlight">software systems</span> for workflow
+                optimization and operational efficiency. Maintain high standards
+                in daily operations while ensuring customer satisfaction.
+                Utilize <span className="highlight">multitasking</span> and{" "}
+                <span className="highlight">team management</span> abilities,
+                fostering skills transferable to{" "}
+                <span className="highlight">software engineering</span> and{" "}
+                <span className="highlight">project management</span>.
+              </dd>
+            </dl>
+          </div>
+
+          <div
+            className={`resume-box1-inner-content${
+              work === 2 ? "-active" : ""
+            }`}
+          >
+            <dl>
+              <dd>
+                At <span className="highlight">CargoLedger</span>, a premier
+                logistics firm known for{" "}
+                <span className="highlight">blockchain solutions</span>, I
+                researched{" "}
+                <span className="highlight">consensus mechanisms</span> with an
+                emphasis on partial migration strategies. I sharpened my skills
+                in <span className="highlight">Solidity</span>,{" "}
+                <span className="highlight">smart contract development</span>,
+                and{" "}
+                <span className="highlight">
+                  Ethereum-Polygon communication
+                </span>
+                , contributing to the company's advanced blockchain integration
+                in logistics.
+              </dd>
+            </dl>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Resume;
